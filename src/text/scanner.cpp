2a43167ade4914b970a94077c660167bc702f969
hynguyen <hynguyen@hy-dev-centos>
2017-08-19 16:41:04 -0700

    Add initial scanner files to the text package

---
+++ src/text/scanner.cpp
@@ -0,0 +1,17 @@
+/*
+ * scanner.h
+ *
+ *  Created on: Aug 21, 2017
+ *      Author: Hy Nguyen [hynguyen@daise-labs.com]
+ * 
+ *  Copyright (c) Daise Labs 2017.  All rights reserved. 
+ */
+
+#include <daiselabs/text/scanner.h>
+
+namespace daiselabs {
+namespace text {
+
+    
+} /* namespace text */ 
+} /* namespace daiselabs */
